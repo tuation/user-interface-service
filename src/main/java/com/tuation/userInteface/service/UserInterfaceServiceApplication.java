86cadf3e2adf4d7ecc185955d25d21f0835b48e4
Debasis Mohapatra <ds.tuation@gmail.com>
2020-01-15 10:47:47 +0530

    added main and test class

---
+++ src/main/java/com/tuation/userInteface/service/UserInterfaceServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tuation.userInteface.service;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserInterfaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserInterfaceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuation/userInteface/service/UserInterfaceServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.tuation.userInteface.service;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class  UserInterfaceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
